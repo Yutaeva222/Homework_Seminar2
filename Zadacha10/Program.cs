8755871f615187bfa0ad8d22fee73a926924dad5
Maria Yutaeva <mariya1982@yandex.ru>
2022-09-22 15:18:38 +0300

    Задача10

---
+++ Zadacha10/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SecondDigit(int number) 
+{
+    int Dec = number / 100;
+    int Sot = number / 10 % 10;  
+    return Sot;
+}
+int secondDig = SecondDigit(number);
+Console.WriteLine("Вторая цифра этого числа " + secondDig);
+
+
